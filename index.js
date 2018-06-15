b9baa067b512b4e29c7ba50d2441030ad8c3e93d
yizhaoyang <yizhaoyang1224@gmail.com>
2018-06-15 13:59:00 +0200

    11

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/mysecret', (req, res) => res.send('This is a confidential page.'))
-app.get('/home', (req, res) => res.sendFile('/Users/David/Documents/photography/content/photography.html'))
+app.get('/home', (req, res) => res.sendFile('/Users/David/Documents/photography/home/photography.html'))
 app.use(express.static('content'))
 
 
