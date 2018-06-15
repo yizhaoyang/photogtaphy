4e5ee49d38f778dd40191826bfb163524e3da42a
yizhaoyang <yizhaoyang1224@gmail.com>
2018-06-15 17:01:13 +0200

    oio

--- index.js
+++ index.js
@@ -2,18 +2,26 @@ const express = require('express')
 const app = express()
 
 // create database
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017";
+var MongoClient = require('mongodb').MongoClient
+, format = require('util').format;
 
-MongoClient.connect(url, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("mydb");
-    dbo.createCollection("requests").find({}).toArray(function(err, res) {
-      if (err) throw err;
-      console.log("Collection created!");
-      db.close();
+MongoClient.connect('mongodb://127.0.0.1:27017/test', function(err, db) {
+if(err) throw err;
+
+var collection = db.collection('initiators');
+collection.insert({a:2}, function(err, docs) {
+    collection.count(function(err, count) {
+        console.log(format("count = %s", count));
     });
-  });
+});
+
+// Locate all the entries using find
+collection.find().toArray(function(err, results) {
+    console.dir(results);
+    // Let's close the db
+    db.close();
+});
+});
 
 
 
