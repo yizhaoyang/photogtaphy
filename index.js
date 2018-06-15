24a52289b9871c31d1099eb20ffd225ffa674e52
yizhaoyang <yizhaoyang1224@gmail.com>
2018-06-15 14:24:40 +0200

    request

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express()
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.get('/mysecret', (req, res) => res.send('This is a confidential page.'))
-app.get('/home', (req, res) => res.sendFile('/Users/David/Documents/photography/home/photography.html'))
-app.use(express.static('content'))
+app.get('/home', (req, res) => res.sendFile('/Users/David/Documents/photography/home/home.html'))
+app.get('/request', (req, res) => res.sendFile('/Users/David/Documents/photography/home/request.html'))
+app.use(express.static('home'))
 
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
